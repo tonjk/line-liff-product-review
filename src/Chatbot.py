c5cceaef7ebe0f204202d0a00eac46dd77d5e731
tonjk <jukkrit.m@merudy.com>
2025-03-26 14:15:00 +0700

    add langfuse and fix prompt

--- src/Chatbot.py
+++ src/Chatbot.py
@@ -1,15 +1,17 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
-# import time, json, redis, os
+import os
 from dotenv import load_dotenv
 load_dotenv()
-
+from langfuse.callback import CallbackHandler
 from .RedisHistory.redis_chat_manager import ChatHistoryManager
+
+langfuse_handler = CallbackHandler(public_key=os.getenv('LANGFUSE_PUBLIC_KEY'), secret_key=os.getenv('LANGFUSE_SECRET_KEY'), host=os.getenv('LANGFUSE_HOST'))
 
 llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=1, max_tokens=1024)
 
-system_prompt = "You are helpful assistant that can chat with user in Thai. You love to talk with user about everything. Use 'ครับ' to respon politely."
+system_prompt = "You are helpful assistant that can chat with user in Thai. You love to talk with user about everything. Use 'ครับ' to respon politely. Respond in short and do not use BOLD text format."
 prompt = ChatPromptTemplate.from_messages([("system", system_prompt),
                                            MessagesPlaceholder(variable_name="history"),
                                            ("human", "{input}")])
@@ -18,7 +20,7 @@ chain = prompt | llm | StrOutputParser()
 # init chat manager
 chat_history_manager = ChatHistoryManager()
 
-def chat(session_id:str, user_input:str):
+def chat(session_id:str, user_name:str, user_input:str):
     user_input = user_input.strip()
     cnt_chat = chat_history_manager.cnt_chat_history(session_id)
     if user_input.lower() == '#reset' or cnt_chat > 40:
@@ -34,7 +36,10 @@ def chat(session_id:str, user_input:str):
         chat_history_manager.save_message(user_id=session_id, role='user', message=user_input)
         response = chain.invoke({"input": user_input,
                                 "history": chat_history_manager.get_recent_chat_history(session_id)},
-                                config={"configurable": {"session_id": session_id}})
+                                config={"callbacks": [langfuse_handler],
+                                        "configurable": {"session_id": session_id,
+                                                         "langfuse_session_id": session_id,
+                                                         "langfuse_user_id": user_name,}})
         chat_history_manager.save_message(user_id=session_id, role='assistant', message=response)
 
         return response
@@ -45,4 +50,4 @@ if __name__=="__main__":
         user_input = input("User: ")
         if user_input == 'exit':
             break
-        print(chat(session_id='123', user_input=user_input))
+        print(chat(session_id='123', user_name='BotTest', user_input=user_input))
